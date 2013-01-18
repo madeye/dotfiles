086a369d843089870ea436f8491e367b8c5b52b4
Max Lv <max.c.lv@gmail.com>
2013-01-18 10:41:49 +0800

    update emacs themes

--- emacs.d/init-local.el
+++ emacs.d/init-local.el
@@ -3,6 +3,13 @@
 (setq auto-save-list-file-name  nil) ; Don't want any .saves files
 (setq auto-save-default         nil) ; Don't want any auto saving
 
+(setq save-place-file "~/.emacs.d/saveplace") ;; keep my ~/ clean
+(setq-default save-place t)                   ;; activate it for all buffers
+(require 'saveplace)                          ;; get the package
+
 (require 'sr-speedbar)
+(menu-bar-mode -1)
+(global-linum-mode t)
+(setq linum-format "%3d \u2502")
 
 (provide 'init-local)
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -92,6 +92,19 @@
 ;;----------------------------------------------------------------------------
 (require 'init-locales)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default)))
+ '(session-use-package t nil (session)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 ;; Local Variables:
 ;; no-byte-compile: t
 ;; End:
